ddd9ca14d26360b0c2db026dbab8e23c3a350ed2
Formateur1 <enseignant.formateur@protonmail.com>
2023-03-24 14:34:12 +0100

    ajout du tableau

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,4 +13,30 @@ export class AppComponent {
   verif:boolean = false;
 
   element:number = 15;
+
+  etudiants:any[] = [
+    {"nom":"Dupont", "prenom":"Louis", "age":28},
+    {"nom":"Toto", "prenom":"Soohie", "age":22},
+    {"nom":"Titi", "prenom":"Marie", "age":33},
+    {"nom":"Durant", "prenom":"Joel", "age":27}
+  ]
+
+  choixClasse(isFirst:boolean, isLast:boolean, isEven:boolean)
+  {
+    var ma_classe:string="";
+    if(isFirst || isLast)
+    {
+      ma_classe = "first_last ";
+    }
+    if(isEven)
+    {
+      ma_classe += "ligne_even";
+    }
+    else
+    {
+      ma_classe += "ligne_odd";
+    }
+    return ma_classe;
+  }
+
 }
