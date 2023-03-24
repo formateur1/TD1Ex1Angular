b6fcb168a4d8e709c636d2eb7f603854feb18e1c
Formateur1 <enseignant.formateur@protonmail.com>
2023-03-24 11:58:09 +0100

    Ajout du nom et du titre

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'TD1Ex1';
+  title = 'Le titre de mon application';
+
+  nom:string = "Jean";
 }
